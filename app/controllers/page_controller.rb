c416041d801397fb60acc046767e0505b635ef20
protege <o.amous@yahoo.com>
2025-09-15 11:45:43 +0100

    alpha blog is working

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,12 @@
+# app/controllers/application_controller.rb
+class ApplicationController < ActionController::Base
+end
+
+# app/controllers/page_controller.rb
+class PageController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+end
